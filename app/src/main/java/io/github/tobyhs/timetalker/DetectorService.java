d57b2adbfb5a858e6255561f0e663fddfbb8340f
Toby Hsieh <thrasher13b@gmail.com>
2016-10-30 14:55:40 -0700

    Implement DetectorService#hearShake

--- app/src/main/java/io/github/tobyhs/timetalker/DetectorService.java
+++ app/src/main/java/io/github/tobyhs/timetalker/DetectorService.java
@@ -1,15 +1,73 @@
 package io.github.tobyhs.timetalker;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.SensorManager;
+import android.os.Handler;
 import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
+
+import com.squareup.seismic.ShakeDetector;
 
 /**
  * This is a service that detects the appropriate event to trigger the phone to speak the time.
  */
-public class DetectorService extends Service {
+public class DetectorService extends Service implements ShakeDetector.Listener {
+    public static long LIFETIME_MILLIS = 10000;
+
+    TimeFormatter timeFormatter = new TimeFormatter();
+    TextToSpeech textToSpeech;
+    ShakeDetector shakeDetector;
+    Handler deathHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        shakeDetector = new ShakeDetector(this);
+        shakeDetector.start((SensorManager) getSystemService(Context.SENSOR_SERVICE));
+
+        deathHandler = new Handler();
+        deathHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                stopSelf();
+            }
+        }, LIFETIME_MILLIS);
+    }
+
+    @Override
+    public void onDestroy() {
+        shakeDetector.stop();
+
+        if (textToSpeech != null) {
+            textToSpeech.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void hearShake() {
+        initTextToSpeech();
+        textToSpeech.speak(timeFormatter.format(), TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private synchronized void initTextToSpeech() {
+        if (textToSpeech == null) {
+            Context context = getApplicationContext();
+            textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    // TODO: handle error status
+                }
+            });
+        }
     }
 }
--- app/src/test/java/io/github/tobyhs/timetalker/DetectorServiceTest.java
+++ app/src/test/java/io/github/tobyhs/timetalker/DetectorServiceTest.java
@@ -3,15 +3,48 @@ package io.github.tobyhs.timetalker;
 import android.content.Intent;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowTextToSpeech;
+import org.robolectric.util.ServiceController;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
 public class DetectorServiceTest {
     @Test
     public void onBind_ReturnsNull() {
         DetectorService service = new DetectorService();
         assertThat(service.onBind(new Intent()), is(nullValue()));
+    }
+
+    @Test
+    public void hearShake_SpeaksTime() {
+        ServiceController<DetectorService> serviceController = Robolectric.buildService(DetectorService.class);
+        serviceController.create();
+        DetectorService service = serviceController.get();
+
+        TimeFormatter timeFormatter = mock(TimeFormatter.class);
+        String formattedTime = "9 45 AM";
+        when(timeFormatter.format()).thenReturn(formattedTime);
+        service.timeFormatter = timeFormatter;
+
+        service.hearShake();
+
+        ShadowTextToSpeech ttsShadow = shadowOf(service.textToSpeech);
+        String spokenText = ttsShadow.getLastSpokenText();
+        assertThat(spokenText, is(formattedTime));
+
+        serviceController.destroy();
+        assertThat(ttsShadow.isShutdown(), is(true));
     }
 }
