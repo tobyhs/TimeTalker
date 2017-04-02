57dacf6d3f57c4ac4a605a88af585ec2b0d17394
Toby Hsieh <thrasher13b@gmail.com>
2017-04-01 17:34:31 -0700

    Only start DetectorService when ringer is on

--- app/src/main/java/io/github/tobyhs/timetalker/ScreenActionReceiver.java
+++ app/src/main/java/io/github/tobyhs/timetalker/ScreenActionReceiver.java
@@ -3,6 +3,7 @@ package io.github.tobyhs.timetalker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 
 /**
  * Broadcast receiver that is triggered when the screen turns on and starts {@link DetectorService}
@@ -10,6 +11,10 @@ import android.content.Intent;
 public class ScreenActionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, DetectorService.class));
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        if (audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+            context.startService(new Intent(context, DetectorService.class));
+        }
     }
 }
--- app/src/test/java/io/github/tobyhs/timetalker/ScreenActionReceiverTest.java
+++ app/src/test/java/io/github/tobyhs/timetalker/ScreenActionReceiverTest.java
@@ -2,27 +2,48 @@ package io.github.tobyhs.timetalker;
 
 import android.app.Application;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;;
+import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class ScreenActionReceiverTest {
+    private Application application = RuntimeEnvironment.application;
+    private AudioManager audioManager;
+    private ScreenActionReceiver receiver = new ScreenActionReceiver();
+
+    @Before
+    public void setup() {
+        audioManager = (AudioManager) application.getSystemService(Context.AUDIO_SERVICE);
+    }
+
     @Test
-    public void onReceive_starts_DetectorService() {
-        Application application = RuntimeEnvironment.application;
-        ScreenActionReceiver receiver = new ScreenActionReceiver();
+    public void onReceive_withRingerOn_starts_DetectorService() {
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
         receiver.onReceive(application, new Intent(Intent.ACTION_SCREEN_ON));
 
         Intent serviceIntent = shadowOf(application).getNextStartedService();
         ComponentName componentName = new ComponentName(application, DetectorService.class);
         assertThat(serviceIntent.getComponent(), is(componentName));
+    }
+
+    @Test
+    public void onReceive_withRingerOff_doesNotStart_service() {
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+        receiver.onReceive(application, new Intent(Intent.ACTION_SCREEN_ON));
+
+        Intent serviceIntent = shadowOf(application).getNextStartedService();
+        assertThat(serviceIntent, is(nullValue()));
     }
 }
