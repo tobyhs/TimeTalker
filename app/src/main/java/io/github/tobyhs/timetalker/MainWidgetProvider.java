d0b2bd745d3a821c64b69736d9a299be1d2a4121
Toby Hsieh <thrasher13b@gmail.com>
2018-01-10 14:42:53 -0800

    Add bare home screen widget
    This will be used later to start ScreenService when enabled (to work
    around Android 8.0's new background service limitations).

---
+++ app/src/main/java/io/github/tobyhs/timetalker/MainWidgetProvider.java
@@ -0,0 +1,9 @@
+package io.github.tobyhs.timetalker;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * A home screen widget that starts {@link ScreenService} when enabled.
+ */
+public class MainWidgetProvider extends AppWidgetProvider {
+}
---
+++ app/src/test/java/io/github/tobyhs/timetalker/MainWidgetProviderTest.java
@@ -0,0 +1,4 @@
+package io.github.tobyhs.timetalker;
+
+public class MainWidgetProviderTest {
+}
