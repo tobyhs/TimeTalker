ab43a97012a1967635ac206c965bb82596f95d58
Toby Hsieh <thrasher13b@gmail.com>
2018-01-10 15:31:42 -0800

    Implement onEnabled & onDisabled for MainWidgetProvider
    These methods start and stop ScreenService.

--- app/src/main/java/io/github/tobyhs/timetalker/MainWidgetProvider.java
+++ app/src/main/java/io/github/tobyhs/timetalker/MainWidgetProvider.java
@@ -1,9 +1,22 @@
 package io.github.tobyhs.timetalker;
 
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 /**
  * A home screen widget that starts {@link ScreenService} when enabled.
  */
 public class MainWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        context.startService(new Intent(context, ScreenService.class));
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        context.stopService(new Intent(context, ScreenService.class));
+    }
 }
--- app/src/test/java/io/github/tobyhs/timetalker/MainWidgetProviderTest.java
+++ app/src/test/java/io/github/tobyhs/timetalker/MainWidgetProviderTest.java
@@ -1,4 +1,36 @@
 package io.github.tobyhs.timetalker;
 
+import android.app.Application;
+import android.content.ComponentName;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowApplication;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
 public class MainWidgetProviderTest {
+    private MainWidgetProvider widgetProvider = new MainWidgetProvider();
+    private Application application = RuntimeEnvironment.application;
+    private ShadowApplication shadowApplication = shadowOf(application);
+    private ComponentName screenServiceComponent = new ComponentName(application, ScreenService.class);
+
+    @Test
+    public void onEnabled() {
+        widgetProvider.onEnabled(application);
+        ComponentName nextComponent = shadowApplication.getNextStartedService().getComponent();
+        assertThat(nextComponent, is(screenServiceComponent));
+    }
+
+    @Test
+    public void onDisabled() {
+        widgetProvider.onDisabled(application);
+        ComponentName nextComponent = shadowApplication.getNextStoppedService().getComponent();
+        assertThat(nextComponent, is(screenServiceComponent));
+    }
 }
